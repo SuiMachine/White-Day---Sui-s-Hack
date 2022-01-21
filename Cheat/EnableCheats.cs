9b21d3e48b9f9a1d737f8f12eedc4a4c762f942e
Jakub Dylus <SuiMachine@users.noreply.github.com>
2022-01-21 22:53:34 +0100

    Cheat script, you want to enable it

---
+++ Cheat/EnableCheats.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace SuisHack.Cheat
+{
+	[HarmonyPatch]
+	class EnableCheats
+	{
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(PlayerBehaviour), nameof(PlayerBehaviour.OnAwake))]
+		public static void OnAwakePlayerBehaviourPostfix(ref bool ___godMode)
+		{
+			___godMode = true;
+		}
+	}
+}

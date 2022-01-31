f56534e930f0441593afa9196cbcc70588faa21f
Jakub Dylus <SuiMachine@users.noreply.github.com>
2022-01-31 10:29:01 +0100

    Config implemented

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using SuisHack.FPS_SettingsHack;
 
@@ -9,10 +10,21 @@ namespace SuisHack
 	{
 		public static Harmony harmonyInstance { get; private set; }
 		public static BepInEx.Logging.ManualLogSource log;
+		private readonly ConfigEntry<bool> Config_Cheat_GodMode;
+		private readonly ConfigEntry<bool> Config_Cheat_DisableGuardSight;
+
+
+		public Plugin()
+		{
+			Config_Cheat_GodMode = Config.Bind("Cheats", "GodMode", false);
+			Config_Cheat_DisableGuardSight = Config.Bind("Cheats", "DisableGuardsSight", false);
+		}
 
 		private void Awake()
 		{
 			log = Logger;
+
+
 			// Plugin startup logic
 			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
 
@@ -22,8 +34,11 @@ namespace SuisHack
 			Harmony.DEBUG = true;
 #pragma warning restore CS0618 // Type or member is obsolete
 #endif
-			Cheat.EnableCheats.InjectEarly(harmonyInstance);
-			Cheat.SecurityGuardCheat.InjectEarly(harmonyInstance);
+			if(Config_Cheat_GodMode.Value)
+				Cheat.EnableCheats.InjectEarly(harmonyInstance);
+			if (Config_Cheat_DisableGuardSight.Value)
+				Cheat.SecurityGuardCheat.InjectEarly(harmonyInstance);
+
 			FPS_Settings.InjectEarly(harmonyInstance);
 		}
 

ca45e6436694662157e3b1f23d1554bbf2c9f81d
SuiMachine <SuiMachine@users.noreply.github.com>
2022-07-18 20:31:36 +0200

    Just test

--- Source/Cheat/SecurityGuardCheat.cs
+++ Source/Cheat/SecurityGuardCheat.cs
@@ -7,11 +7,9 @@ namespace SuisHack.Cheat
 	{
 		internal static void InjectEarly(Harmony harmonyInstance)
 		{
-			{
-				var originalMethod = typeof(SecurityGuardBehaviour).GetMethod("UpdateCheckActiveAiSight", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-				var detourMethod = typeof(SecurityGuardCheat).GetMethod(nameof(UpdateCheckActiveAiSightDetour), System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
-				harmonyInstance.Patch(originalMethod, postfix: new HarmonyMethod(detourMethod));
-			}
+			var originalMethod = typeof(SecurityGuardBehaviour).GetMethod("UpdateCheckActiveAiSight", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+			var detourMethod = typeof(SecurityGuardCheat).GetMethod(nameof(UpdateCheckActiveAiSightDetour), System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+			harmonyInstance.Patch(originalMethod, postfix: new HarmonyMethod(detourMethod));
 		}
 
 		public static void UpdateCheckActiveAiSightDetour(ref bool ___ActiveSearchingSystem, ref float ___Hearability, ref float ___targetVisibility)
